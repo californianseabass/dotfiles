3cae673cb0b5f125ec5344cb67f30ef7eb86ae58
Eric Sebastian <sebastian@sidewalklabs.com>
2018-01-23 11:07:28 -0500

    emacs init update

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -4,18 +4,28 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
-(setq package-enable-at-startup nil)
+;;(setq package-enable-at-startup nil)
+
 (package-initialize)
-
-(require 'evil)
-(evil-mode t)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+(when (not package-archive-contents)
+  (package-refresh-contents))
 
 (eval-when-compile
   (require 'use-package))
+
+(defvar myPackages
+  '(elpy
+  	evil
+  	flycheck
+    solarized-theme))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+
+;; BASIC CONFIGURATION
 
 ;; This is only needed once, near the top of the file
 (eval-when-compile
@@ -23,9 +33,52 @@
   (add-to-list 'load-path "<path where use-package is installed>")
   (require 'use-package))
 
+(setq inhibit-startup-message t) ;; hide the startup message
+;; https://www.emacswiki.org/emacs/NoTabs
+(setq-default indent-tabs-mode nil) ;; turn tabs into spaces
 
+;; global key bindings
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x l") 'company-jedi)
+
+
+;; company
+(require 'company)
+(use-package company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+;; company-jedi
+(require 'company-jedi)
+(defun my/python-mode-hook ()
+  (add-to-list 'company-backends 'company-jedi))
+(add-hook 'python-mode-hook 'my/python-mode-hook)
+
+;; elpy
+(require 'elpy)
+(elpy-enable)
+
+;; Evil
+(require 'evil)
+(evil-mode t)
+
+
+;; flycheck
+(require 'flycheck)
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; python-mode
+;; https://www.emacswiki.org/emacs/IndentingPython
+(add-hook 'python-mode-hook
+    (lambda ()
+	    (setq-default indent-tabs-mode t)
+	    (setq-default tab-width 4)
+	    (setq-default py-indent-tabs-mode t)
+    (add-to-list 'write-file-functions 'delete-trailing-whitespace)))
 
 ;; Solarized
+(require 'solarized-theme)
 ;; https://github.com/sellout/emacs-color-theme-solarized/pull/187
 (use-package solarized-theme
   :ensure t
@@ -35,15 +88,56 @@
   (setq solarized-scale-org-headlines nil)
   (setq solarized-high-contrast-mode-line t)
   (load-theme 'solarized-dark t))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (solarized-theme use-package evil))))
+ '(package-selected-packages
+   (quote
+    (web-mode magit tide solarized-theme use-package evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+
+
+
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; enable typescript-tslint checker
+(flycheck-add-mode 'typescript-tslint 'web-mode)
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+
+
