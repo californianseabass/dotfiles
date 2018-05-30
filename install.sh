90a05f20a9bc013b485a7c6ae6db14269bc89b52
Eric Sebastian Soto <releasetheseabass@gmail.com>
2018-05-29 22:46:55 -0400

    quote strings in install script

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 # bash_profile=$HOME/.bash_profile
-gitignore_global=$HOME/.gitignore_global
-tmux_conf=$HOME/.tmux.conf
-tmuxinator=$HOME/.tmuxinator
-emacs=$HOME/.emacs.d
+set -o nounset
+gitignore_global="$HOME/.gitignore_global"
+tmux_conf="$HOME/.tmux.conf"
+tmuxinator="$HOME/.tmuxinator"
+emacs="$HOME/.emacs.d"
 
 while getopts "r" opt
 do
     case "$opt" in
         r)
-            echo "choosing to reinstall";
+            echo "choosing to reinstall"
             # rm $bash_profile;
-            rm $tmux_conf;
-            rm -rf $tmuxinator;
-            rm $gitignore_global;
-            rm -rf $emacs;
+            rm $tmux_conf
+            rm -rf $tmuxinator
+            rm "$gitignore_global"
+            rm -rf "$emacs"
             ;;
         *)
             echo "Wrong command line argument. Exiting...";
@@ -22,8 +23,8 @@ do
     esac
 done
 
-ln -s $PWD/gitignore_global $gitignore_global
-ln -s $PWD/tmux_conf $tmux_conf
+ln -s "$PWD/gitignore_global" "$gitignore_global"
+ln -s "$PWD/tmux_conf" "$tmux_conf"
 # ln -s $PWD/bash_profile $bash_profile
-ln -s $PWD/tmuxinator $tmuxinator
-ln -s $PWD/emacs.d $emacs
+ln -s "$PWD/tmuxinator" "$tmuxinator"
+ln -s "$PWD/emacs.d" "$emacs"
