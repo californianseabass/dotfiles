2ec33c0cfdeb891327ef709c42285ad53d8d9f88
Eric Sebastian <sebastian@sidewalklabs.com>
2019-08-08 15:09:14 -0400

    update typescript for pebble installer style

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -326,8 +326,9 @@
 
 (require 'prettier-js)
 (setq prettier-js-args '(
-  "--print-width" 100
-  "--trailing-comma" "none"
+  "--print-width" "100"
+  "--trailing-comma" "es5"
+  "--tab-width" "2"
   "--bracket-spacing" "true"
   "--arrow-parens" "avoid"
   "--use-tabs" "false"
@@ -442,13 +443,15 @@
   (flycheck-mode +1)
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (eldoc-mode +1)
-  (add-hook 'typescript-mode-hook 'prettier-js-mode)
+  (prettier-js-mode +1)
   (tide-hl-identifier-mode +1)
   ;; company is an optional dependency. You have to
   ;; install it separately via package-install
   ;; `M-x package-install [ret] company`
   (company-mode +1)
-  (add-hook 'before-save-hook #'tide-format-before-save)
+
+   (setq-default typescript-indent-level 2)
+  ;;(add-hook 'before-save-hook #'tide-format-before-save) maybe interferes with pretteir?
   (local-set-key (kbd "M-.") #'tide-jump-to-definition))
 
 (require 'web-mode)
